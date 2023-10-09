23b9df3a120f88677d3043a8aa7f9b5aa3e53686
Christopher <chrshih875@gmail.com>
2023-10-09 14:52:36 -0700

    setting up React

---
+++ React/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+          <div className="container">
+            <Route>
+
+            </Route>
+          </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
