5d79a268f5fc18d0d59a2eef3cb28bf5f7301a8e
Christopher <chrshih875@gmail.com>
2023-10-10 14:53:10 -0700

    trying to add a search bar

--- React/src/components/TrailFinder.js
+++ React/src/components/TrailFinder.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const TrailFinder = () => {
-    const [Locations, setLocationsList] = useState([]);
+export const TrailFinder = ({ onSearch }) => {
     const [Trails, setTrails] = useState([]);
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
 
     const getTrails = async (Locations) => {
         axios.get(`http://localhost:7080/trails?latitude=${Locations.data.origin.latitude}&longitude=${Locations.data.origin.longitude}`)
@@ -11,14 +12,32 @@ export const TrailFinder = () => {
         .catch((err) => {console.log(err)});
     }
 
-    useEffect(() => {
-        const getDistance = async (info) => {
-            axios.get(`http://localhost:7080/direction?Origin=${info.lat}&Endpoint=${info.long}`)
-            .then((response) => setLocationsList(response.data.json()))
-            .catch((err) => {console.log(err)});
-        }
-        getDistance()
-    }, [])
+    const getDistance = async (info) => {
+        axios.get(`http://localhost:7080/direction?Origin=${info.lat}&Endpoint=${info.long}`)
+        .then((response) => getTrails(response.data.json()))
+        .catch((err) => {console.log(err)});
+    }
 
+    const handleSearch = () => {
+        onSearch({ lat: latitude, long: longitude });
+      };
+
+      return (
+        <div>
+          <input
+            type="text"
+            placeholder="Latitude"
+            value={latitude}
+            onChange={(e) => setLatitude(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Longitude"
+            value={longitude}
+            onChange={(e) => setLongitude(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+      );
 
 }
