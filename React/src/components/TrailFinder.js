8cfab593d91e5d297fc73364e25451e84e0f24ec
Christopher <chrshih875@gmail.com>
2023-10-11 18:49:54 -0700

    adding child component - Search Form

--- React/src/components/TrailFinder.js
+++ React/src/components/TrailFinder.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { SearchForm } from './SearchForm';
 export const TrailFinder = ({ onSearch }) => {
     const [Trails, setTrails] = useState([]);
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
 
     const getTrails = async (Locations) => {
         axios.get(`http://localhost:7080/trails?latitude=${Locations.data.origin.latitude}&longitude=${Locations.data.origin.longitude}`)
@@ -17,27 +15,11 @@ export const TrailFinder = ({ onSearch }) => {
         .then((response) => getTrails(response.data.json()))
         .catch((err) => {console.log(err)});
     }
-
-    const handleSearch = () => {
-        onSearch({ lat: latitude, long: longitude });
-      };
-
-      return (
-        <div>
-          <input
-            type="text"
-            placeholder="Latitude"
-            value={latitude}
-            onChange={(e) => setLatitude(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Longitude"
-            value={longitude}
-            onChange={(e) => setLongitude(e.target.value)}
-          />
-          <button onClick={handleSearch}>Search</button>
-        </div>
-      );
-
+    return (
+      <div>
+        <h1>Trail Finder</h1>
+        <SearchForm onSearch={handleSearch} />
+        {/* Display the Trails and Locations based on API data */}
+      </div>
+    );
 }
