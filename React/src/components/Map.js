334f5e9b35e97b20f7626fa3d1c1d458f33c6691
Christopher <chrshih875@gmail.com>
2023-10-14 14:03:46 -0700

    testing more of leaflet library

--- React/src/components/Map.js
+++ React/src/components/Map.js
@@ -6,16 +6,16 @@ import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 function MyMapComponent() {
     useEffect(() => {
       // Create a Leaflet map
-      if (!L.DomUtil.get('map')) { //to check if the map has been initilaized
-      const map = L.map('map').setView([51.505, -0.09], 13);
+    //   if (!L.DomUtil.get('map')) { //to check if the map has been initilaized
+    //   const map = L.map('map').setView([51.505, -0.09], 13);
 
-      // Create a tile layer for the map
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(map);
-    }
-    }, []); // The empty dependency array ensures this code runs once when the component is mounted
+    //   // Create a tile layer for the map
+    //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     maxZoom: 19,
+    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    //   }).addTo(map);
+    // }
+    // }, []); // The empty dependency array ensures this code runs once when the component is mounted
 
     return <div id="map" style={{ width: '100%', height: '500px' }}></div>;
   }
