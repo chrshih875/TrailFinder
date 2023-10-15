8dfc790b4986d7cf6b73402da448f8f48c4c0a0e
Christopher <chrshih875@gmail.com>
2023-10-15 12:39:58 -0700

    attempting to check if the leaflet map has been initlized through true false statement in the useEffect

--- React/src/components/Map.js
+++ React/src/components/Map.js
@@ -1,23 +1,25 @@
-import React, { useEffect } from 'react';
-import L from 'leaflet'; // Import Leaflet
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
-
+import React, { useEffect, useState } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function MyMapComponent() {
-    useEffect(() => {
-      // Create a Leaflet map
-      if (!L.DomUtil.get('map')) { //to check if the map has been initilaized
+  const [mapInitialized, setMapInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!mapInitialized) {
       const map = L.map('map').setView([51.505, -0.09], 13);
 
-      // Create a tile layer for the map
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
+
+      // Update the state to indicate that the map has been initialized
+      setMapInitialized(true);
     }
-    }, []); // The empty dependency array ensures this code runs once when the component is mounted
+  }, [mapInitialized]);
 
-    return <div id="map" style={{ width: '100%', height: '500px' }}></div>;
-  }
+  return <div id="map" style={{ width: '100%', height: '500px' }}></div>;
+}
 
-  export default MyMapComponent;
+export default MyMapComponent;
