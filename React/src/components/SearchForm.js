aa3848209d5bf7e8cd61aa58eff85a3cc9660ea6
Christopher <chrshih875@gmail.com>
2023-11-07 22:25:36 -0800

    tryna get the load spinner to last until data from the api is retrieved

--- React/src/components/SearchForm.js
+++ React/src/components/SearchForm.js
@@ -8,22 +8,22 @@ export const SearchForm = ({ onSearch }) => {
     onSearch({ lat: origin, long: destination });
   };
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Origin"
-        value={origin}
-        onChange={(e) => setOrigin(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Destination"
-        value={destination}
-        onChange={(e) => setDestination(e.target.value)}
-      />
-      <p></p>
-      <button onClick={handleSearch}>Search</button>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <input
+  //       type="text"
+  //       placeholder="Origin"
+  //       value={origin}
+  //       onChange={(e) => setOrigin(e.target.value)}
+  //     />
+  //     <input
+  //       type="text"
+  //       placeholder="Destination"
+  //       value={destination}
+  //       onChange={(e) => setDestination(e.target.value)}
+  //     />
+  //     <p></p>
+  //     <button onClick={handleSearch}>Search</button>
+  //   </div>
+  // );
 };
